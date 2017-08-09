70349877fc2bb2b3991597216d47e6b57a0a2f51
Alyssa Nickow <alyssa@nickow.com>
2017-08-09 13:08:09 -0500

    completed Dojo Mail activity

--- Dojo_Mail/src/app/app.module.ts
+++ Dojo_Mail/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
 
 import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
 import { HttpModule } from '@angular/http'; // <-- Import HttpModule
@@ -10,7 +9,6 @@ import { HttpModule } from '@angular/http'; // <-- Import HttpModule
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
   ],
   imports: [
     BrowserModule,
